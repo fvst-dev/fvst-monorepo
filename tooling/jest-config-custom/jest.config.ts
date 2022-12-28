dbce5cb12eaeabad58d58cc6d9c35c15223165ca
Magnus Muru <murumagnus@gmail.com>
2022-12-28 11:31:58 +0200

    fix: lint fix for jest config

--- tooling/jest-config-custom/jest.config.ts
+++ tooling/jest-config-custom/jest.config.ts
@@ -1,11 +1,10 @@
-import type { Config } from "jest";
-
+import type { Config } from 'jest';
 
 const config: Config = {
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 
   projects: ['<rootDir>', '<rootDir>/apps/*'],
@@ -16,9 +15,9 @@ const config: Config = {
   collectCoverage: true,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8"
+  coverageProvider: 'v8',
 };
 export default config;

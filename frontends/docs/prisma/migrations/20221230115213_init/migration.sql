db121c5a84e621b57de47ae6416e9ae66f0ed9af
Magnus Muru <murumagnus@gmail.com>
2022-12-30 16:17:47 +0200

    added prettier ignore unknown to format

---
+++ frontends/docs/prisma/migrations/20221230115213_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);

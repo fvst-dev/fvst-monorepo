d8f8809b3868c8732f55c9aade9629edd313a31f
Dennis Nikiforov <24647964+dennisnikiforov@users.noreply.github.com>
2023-02-03 16:39:16 +0200

    wf changes

---
+++ apps/apollo-gateway/publish_ci_schema_updates.sh
@@ -0,0 +1,24 @@
+cd apps
+
+for d in */ ; do
+    APP_NAME=$(echo $d | sed 's/.$//')
+    SCHEMA_FILE=$APP_NAME/src/graphql/schema/schema.graphql
+
+    BRANCH_OR_PR="pr-21"
+
+    GRAPH_REF="fvst-main@$BRANCH_OR_PR"
+
+    ENDPOINT="https://$APP_NAME-fvst-monorepo-$BRANCH_OR_PR.up.railway.app"
+
+
+    if [ ! -f "$SCHEMA_FILE" ]; then 
+        continue;
+    fi
+
+    echo "=============================================================="
+    echo "App $APP_NAME is configured to use endpoint $ENDPOINT and this schema file $SCHEMA_FILE"
+
+    rover subgraph publish $GRAPH_REF --routing-url $ENDPOINT--schema $SCHEMA_FILE --name $APP_NAME
+
+    echo "=============================================================="
+done

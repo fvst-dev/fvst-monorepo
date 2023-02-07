ef2294e2ae85655174ed034f4d40cb6dc2030785
Dennis Nikiforov <24647964+dennisnikiforov@users.noreply.github.com>
2023-02-07 09:58:21 +0200

    use more secrets

--- apps/apollo-gateway/publish_ci_schema_updates.sh
+++ apps/apollo-gateway/publish_ci_schema_updates.sh
@@ -4,9 +4,7 @@ for d in */ ; do
     APP_NAME=$(echo $d | sed 's/.$//')
     SCHEMA_FILE=$APP_NAME/src/graphql/schema/schema.graphql
 
-    GRAPH_REF="fvst-main@pr-$PR_NUMBER"
-
-    ENDPOINT="https://service-$APP_NAME-fvst-monorepo-pr-$PR_NUMBER.up.railway.app"
+    ENDPOINT="https://service-$APP_NAME-$RAILWAY_PROJECT_NAME-pr-$PR_NUMBER.up.railway.app"
 
     if [ ! -f "$SCHEMA_FILE" ]; then 
         continue;
@@ -15,7 +13,7 @@ for d in */ ; do
     echo "=============================================================="
     echo "App $APP_NAME is configured to use endpoint $ENDPOINT and this schema file $SCHEMA_FILE"
 
-    ~/.rover/bin/rover subgraph publish $GRAPH_REF --routing-url $ENDPOINT --schema $SCHEMA_FILE --name $APP_NAME
+    ~/.rover/bin/rover subgraph publish $APOLLO_GRAPH_REF --routing-url $ENDPOINT --schema $SCHEMA_FILE --name $APP_NAME
 
     echo "=============================================================="
 done

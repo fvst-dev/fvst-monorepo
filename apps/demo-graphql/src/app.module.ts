5272175554a7afc49ed8d2e6c94bb82189d2eb3a
Magnus Muru <murumagnus@gmail.com>
2023-05-04 17:17:43 +0300

    fix: FVS-33 generate GraphQL schema and enable Apollo Sandbox

--- apps/demo-graphql/src/app.module.ts
+++ apps/demo-graphql/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoService } from './todo.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TodoResolver } from './todo.resolver';
@@ -10,20 +8,21 @@ import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaClient } from '@~internal/prisma_demo/client';
 import { PrismaService } from '@libs/prisma/dist/prisma.service';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
-      autoSchemaFile: true,
-      playground: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: 'src/schema.graphql',
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
       introspection: process.env.NODE_ENV !== 'production',
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     TodoService,
     TodoResolver,
     JwtStrategy,

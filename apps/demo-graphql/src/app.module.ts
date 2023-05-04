0d792854216b41b15db4e600c6f89c63609948c1
Magnus Muru <murumagnus@gmail.com>
2023-05-04 17:42:05 +0300

    fix: added file content

--- apps/demo-graphql/src/app.module.ts
+++ apps/demo-graphql/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TodoService } from './todo.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TodoResolver } from './todo.resolver';
@@ -21,8 +23,9 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
       introspection: process.env.NODE_ENV !== 'production',
     }),
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [
+    AppService,
     TodoService,
     TodoResolver,
     JwtStrategy,

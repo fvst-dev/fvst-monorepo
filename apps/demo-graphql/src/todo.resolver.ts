1da24b27a6c383c28213f878c0d1b460444b49c0
bilal4 <md.bilaljaved@gmail.com>
2023-06-02 14:41:24 +0300

    chore: JwtAuthGuard at service level

--- apps/demo-graphql/src/todo.resolver.ts
+++ apps/demo-graphql/src/todo.resolver.ts
@@ -6,16 +6,15 @@ import { UseGuards } from '@nestjs/common';
 import { CurrentUser } from './current-user.decorator';
 import { User } from './types';
 
+@UseGuards(JwtAuthGuard)
 @Resolver(() => Todo)
 export class TodoResolver {
   constructor(private todoService: TodoService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Query(() => [Todo])
   async todos() {
     return this.todoService.findAll();
   }
-  @UseGuards(JwtAuthGuard)
   @Mutation(() => Todo)
   async createTodo(
     @Args('input') input: CreateTodoInput,
@@ -26,7 +25,6 @@ export class TodoResolver {
       userId: user.userId,
     });
   }
-  @UseGuards(JwtAuthGuard)
   @Mutation(() => Todo)
   async updateTodo(@Args('input') input: UpdateTodoInput): Promise<Todo> {
     return this.todoService.update(input.id, {
@@ -34,7 +32,6 @@ export class TodoResolver {
       completed: input.completed,
     });
   }
-  @UseGuards(JwtAuthGuard)
   @Mutation(() => Todo)
   async deleteTodo(@Args('id', { type: () => Int }) id: number) {
     return this.todoService.delete(id);

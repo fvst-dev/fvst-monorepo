d0096f5a0e2e4b91f8a28ccc09dc702bd8e02a1f
bilal4 <md.bilaljaved@gmail.com>
2023-06-06 03:17:40 +0300

    fix: gateway in web

--- apps/todo-graphql/src/app.module.ts
+++ apps/todo-graphql/src/app.module.ts
@@ -4,19 +4,21 @@ import { AppService } from './app.service';
 import { TodoService } from './todo.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TodoResolver } from './todo.resolver';
-import { ApolloDriver } from '@nestjs/apollo';
 import { JwtAuthGuard } from './auth.guard';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { PrismaService } from './prisma.service';
-
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      autoSchemaFile: 'src/schema.graphql',
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: { path: 'src/schema.graphql', federation: 2 },
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       introspection: process.env.NODE_ENV !== 'production',

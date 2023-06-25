d716ff223fe92e6fc61637aa7b33f0383d626bab
Peeter Tomberg <peeter@fvst.dev>
2023-06-26 01:00:00 +0300

    chore: working on terraform integration

--- infra/modules/google-services/main.tf
+++ infra/modules/google-services/main.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "gcp_resource_manager_api" {
+  service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = true
+}
+
 resource "google_project_service" "containerregistry" {
   service = "containerregistry.googleapis.com"
   disable_on_destroy = true

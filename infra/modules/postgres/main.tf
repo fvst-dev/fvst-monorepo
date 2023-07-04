363ef79ef5d68e144a54c582e67bc365cbf83c8c
Peeter Normak <peeter.normak@fvst.dev>
2023-07-04 16:58:55 +0300

    Add IAM to postgres

--- infra/modules/postgres/main.tf
+++ infra/modules/postgres/main.tf
@@ -7,5 +7,16 @@ resource "google_sql_database_instance" "postgres" {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
     tier = var.tier
+
+    database_flags {
+      name  = "cloudsql.iam_authentication"
+      value = "on"
+    }
   }
+}
+
+resource "google_sql_user" "users" {
+  instance = google_sql_database_instance.postgres.name
+  name     = "me"
+  type = "CLOUD_IAM_SERVICE_ACCOUNT"
 }

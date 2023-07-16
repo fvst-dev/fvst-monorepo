8374f579f05ade3208d69e3f29d4d10720b44934
Peeter Tomberg <peeter@fvst.dev>
2023-07-17 02:09:25 +0300

    chore: moved storage/cache stuff over to the same region as github runners

--- packages/cli/src/commands/infra/create-terraform-bucket.ts
+++ packages/cli/src/commands/infra/create-terraform-bucket.ts
@@ -1,6 +1,7 @@
 import { safeExec } from '../../utils/safeExec';
 import { createIamArgument } from './arguments/createIamArgument';
 import { Command } from '@commander-js/extra-typings';
+import { githubRunnerRegionGroup, githubRunnerRegionList } from './constants/github-runner-regions';
 
 export const createTerraformBucket = new Command()
   .command('create-terraform-bucket')
@@ -10,6 +11,10 @@ export const createTerraformBucket = new Command()
     const bucket = `${iam.project}-terraform-state-${Math.random().toString(36).substring(2, 10)}`;
     console.log(`Creating bucket ${bucket} for terraform state for project ${iam.project}`);
     safeExec(`gcloud services enable storage.googleapis.com --project ${iam.project}`);
-    safeExec(`gcloud storage buckets create 'gs://${bucket}' --project ${iam.project}`);
+    safeExec(
+      `gcloud storage buckets create 'gs://${bucket}' --project ${
+        iam.project
+      } --location=${githubRunnerRegionGroup} --placement=${githubRunnerRegionList.join(',')}`
+    );
     safeExec(`gh variable set 'FVST_PROJECT_TF_STATE_BUCKET_${iam.environment.toUpperCase()}' --body ${bucket}`);
   });
--- packages/cli/src/commands/infra/init.ts
+++ packages/cli/src/commands/infra/init.ts
@@ -30,7 +30,7 @@ export const init = new Command()
         {
           type: 'autocomplete',
           name: 'region',
-          initial: 'eu-west-1',
+          initial: 'europe-west1',
           message: 'In what region should we create the environments in?',
           choices: regions.map((v) => ({
             title: v,
@@ -111,7 +111,7 @@ export const init = new Command()
         safeExec(`fvst infra create-service-account-keys ${iam}`, false);
         safeExec(`fvst infra create-terraform-bucket ${iam}`, false);
         safeExec(`fvst infra create-turborepo-bucket ${iam}`, false);
-        safeExec(`fvst infra create-container-registry ${iam} ${region}`, false);
+        safeExec(`fvst infra create-container-registry ${iam}`, false);
       } catch (e) {
         console.error('Failed configuring project access, deleting created project');
         safeExec(`gcloud projects delete ${project} --quiet`, false);
@@ -124,9 +124,6 @@ export const init = new Command()
       `gh variable set FVST_PROJECT_PREFIX --body '${prefix}'`,
       `gh variable set FVST_PROJECT_REGION --body '${region}'`,
       `gh secret set GH_TOKEN --app actions --body '${githubAuthToken}'`,
-      `gh secret set FVST_PGP_SECRET_SIGNING_PASSPHRASE --app actions --body '${Math.random()
-        .toString(36)
-        .substring(2, 10)}'`,
     ];
     commands.map((command) => safeExec(command));
     // TODO: Once this is merged, we should change this code to link to the file on github or even better, run it automatically

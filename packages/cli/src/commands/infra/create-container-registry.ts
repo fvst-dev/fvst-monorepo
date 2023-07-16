107a021de31ea1c9d7d05e2c1ab3bbcbec65c62f
Peeter Tomberg <peeter@fvst.dev>
2023-07-16 18:18:17 +0300

    chore: working on integrating turborepo remote cache

--- packages/cli/src/commands/infra/create-container-registry.ts
+++ packages/cli/src/commands/infra/create-container-registry.ts
@@ -10,6 +10,8 @@ export const createContainerRegistry = new Command()
   .addArgument(createRegionArgument())
   .action((iam, region) => {
     console.log(`Creating artifact registry "registry" for docker images used in project ${iam.project}`);
+    // this is needed for CI to be able to generate tokens for docker to access artifact repository
+    safeExec(`gcloud services enable iamcredentials.googleapis.com --project=${iam.project}`);
     safeExec(`gcloud services enable compute.googleapis.com artifactregistry.googleapis.com --project ${iam.project}`);
     safeExec(
       `gcloud artifacts repositories create 'registry' --repository-format docker --description="Docker repository" --location ${region} --project ${iam.project}`

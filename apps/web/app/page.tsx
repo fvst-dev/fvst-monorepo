1fe255f7eb618cad6d8d4bd0314da927dc00229f
Peeter Tomberg <peeter@fvst.dev>
2023-08-02 23:36:28 +0300

    chore: fixed up npm scripts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -74,6 +74,7 @@ export default async function Home() {
                 <a
                   key={feature.name}
                   href={feature.href}
+                  target="_blank"
                   className=" font-lighth-full flex flex-col items-center justify-center space-y-12 bg-fvst-lilac shadow-xl"
                 >
                   <div className="flex w-full items-center justify-start gap-6">

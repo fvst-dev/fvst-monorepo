6fdc901eb7aaeda5a546669f4be0335ef26436f1
Peeter Tomberg <peeter@fvst.dev>
2023-08-07 00:15:27 +0300

    chore: working on init script

--- infra/envs/staging/main.tf
+++ infra/envs/staging/main.tf
@@ -23,12 +23,12 @@ module "blog-graphql" {
   source   = "../../modules/cloud-run"
   location = var.region
   image    = "${local.registry}/blog-graphql:latest"
-  env = {
-    NODE_ENV: "production",
-    CLERK_ISSUER : module.secrets-manager.CLERK_ISSUER,
-    CLERK_JWSK_URL : module.secrets-manager.CLERK_JWSK_URL,
-    DATABASE_URL : module.postgres.DATABASE_URL_BLOG,
-  }
+  env = [
+    { name: "NODE_ENV", value: "production" },
+    { name: "CLERK_ISSUER", value: module.secrets-manager.CLERK_ISSUER },
+    { name: "CLERK_JWSK_URL", value: module.secrets-manager.CLERK_JWSK_URL },
+    { name: "DATABASE_URL", value: module.postgres.DATABASE_URL_BLOG },
+  ]
   depends_on = [module.secrets-manager, module.google-services, module.postgres]
   annotations = {
     "run.googleapis.com/cloudsql-instances" = module.postgres.CONNECTION_NAME,
@@ -40,12 +40,12 @@ module "todo-graphql" {
   source   = "../../modules/cloud-run"
   location = var.region
   image    = "${local.registry}/todo-graphql:latest"
-  env = {
-    NODE_ENV: "production",
-    CLERK_ISSUER : module.secrets-manager.CLERK_ISSUER,
-    CLERK_JWSK_URL : module.secrets-manager.CLERK_JWSK_URL,
-    DATABASE_URL : module.postgres.DATABASE_URL_TODO,
-  }
+  env = [
+    { name: "NODE_ENV", value: "production" },
+    { name: "CLERK_ISSUER", value: module.secrets-manager.CLERK_ISSUER },
+    { name: "CLERK_JWSK_URL", value: module.secrets-manager.CLERK_JWSK_URL },
+    { name: "DATABASE_URL", value: module.postgres.DATABASE_URL_TODO },
+  ]
   depends_on = [module.secrets-manager, module.google-services, module.postgres]
   annotations = {
     "run.googleapis.com/cloudsql-instances" = module.postgres.CONNECTION_NAME,
@@ -57,12 +57,12 @@ module "user-graphql" {
   source   = "../../modules/cloud-run"
   location = var.region
   image    = "${local.registry}/user-graphql:latest"
-  env = {
-    NODE_ENV: "production",
-    CLERK_ISSUER : module.secrets-manager.CLERK_ISSUER,
-    CLERK_JWSK_URL : module.secrets-manager.CLERK_JWSK_URL,
-    DATABASE_URL : module.postgres.DATABASE_URL_USER,
-  }
+  env = [
+    { name: "NODE_ENV", value: "production" },
+    { name: "CLERK_ISSUER", value: module.secrets-manager.CLERK_ISSUER },
+    { name: "CLERK_JWSK_URL", value: module.secrets-manager.CLERK_JWSK_URL },
+    { name: "DATABASE_URL", value: module.postgres.DATABASE_URL_USER },
+  ]
   depends_on = [module.secrets-manager, module.google-services, module.postgres]
   annotations = {
     "run.googleapis.com/cloudsql-instances" = module.postgres.CONNECTION_NAME,
@@ -75,12 +75,12 @@ module "graphql-gateway" {
   source              = "../../modules/cloud-run"
   location            = var.region
   image               = "${local.registry}/graphql-gateway:latest"
-  env = {
-    NODE_ENV: "production",
-    TODO_SERVICE_URL : "${module.todo-graphql.url}/graphql"
-    BLOG_SERVICE_URL : "${module.blog-graphql.url}/graphql"
-    USER_SERVICE_URL : "${module.user-graphql.url}/graphql"
-  }
+  env = [
+    { name: "NODE_ENV", value: "production" },
+    { name: "TODO_SERVICE_URL", value: "${module.todo-graphql.url}/graphql" },
+    { name: "BLOG_SERVICE_URL", value: "${module.blog-graphql.url}/graphql" },
+    { name: "USER_SERVICE_URL", value: "${module.user-graphql.url}/graphql" },
+  ]
   depends_on = [module.blog-graphql, module.todo-graphql, module.user-graphql, module.google-services]
   allow_public_access = true
 }

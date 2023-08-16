c871d7076f3a332aa76409bb1673dda2e55c27af
Peeter Tomberg <peeter@fvst.dev>
2023-08-16 21:03:47 +0300

    chore: working secrets being setup properly

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,9 @@
 #! /usr/bin/env ts-node
 
 import { Command } from '@commander-js/extra-typings';
+
+import { init as devInit } from './commands/dev/init';
+
 import { init } from './commands/infra/init';
 import { setupProject } from './commands/infra/setup-project';
 import { createServiceAccountKeys } from './commands/infra/create-service-account-keys';
@@ -10,6 +13,7 @@ import { createContainerRegistry } from './commands/infra/create-container-regis
 import { createTurborepoBucket } from './commands/infra/create-turborepo-bucket';
 import { configureTerraformBucketLocally } from './commands/infra/configure-terraform-bucket-locally';
 import { configureTerraformVarsLocally } from './commands/infra/configure-terraform-vars-locally';
+import { setupSecrets } from './commands/infra/setup-secrets';
 
 const program = new Command();
 
@@ -24,8 +28,14 @@ const infra = program.command('infra');
   createContainerRegistry,
   configureTerraformBucketLocally,
   configureTerraformVarsLocally,
+  setupSecrets,
 ].forEach((c) => {
   infra.addCommand(c);
+});
+
+const dev = program.command('dev');
+[devInit].forEach((c) => {
+  dev.addCommand(c);
 });
 
 program.parse();
